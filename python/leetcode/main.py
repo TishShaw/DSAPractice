319a49f52d5f844e056637a6b46383de9fa7b672
Tishtanya Shaw <Tishtanyaa@icloud.com>
2023-10-20 15:30:58 -0400

    Add leetcode problem 238

--- python/leetcode/main.py
+++ python/leetcode/main.py
@@ -79,3 +79,19 @@ def reverseWords(self, s: str) -> str:
         r -= 1
 
     return " ".join(newStr)
+
+
+# 238. Product of Array Except Self
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    n = len(nums)
+    ans = [1] * n
+
+    for i in range(1, n):
+        ans[i] = ans[i - 1] * nums[i - 1]
+
+    suffix = 1
+    for i, num in reversed(list(enumerate(nums))):
+        ans[i] *= suffix
+        suffix *= num
+
+    return ans
